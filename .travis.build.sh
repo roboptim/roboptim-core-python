36b76e706a00648fbcb23412f9e17f31ad408c8a
Thomas Moulard <thomas.moulard@gmail.com>
2013-03-22 03:43:11 +0900

    Fix Travis build script.

--- .travis.build.sh
+++ .travis.build.sh
@@ -8,7 +8,7 @@ install_dir="$root_dir/_travis/install"
 core_dir="$build_dir/roboptim-core"
 
 # Shortcuts.
-git_clone="git clone --quiet --recursive --depth 1"
+git_clone="git clone --quiet --recursive"
 
 # Create layout.
 rm -rf "$build_dir" "$install_dir"
@@ -19,7 +19,7 @@ mkdir -p "$install_dir"
 export PKG_CONFIG_PATH="$install_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 pythonsite_dir=`python -c "import sys, os; print os.sep.join(['lib', 'python' + sys.version[:3], 'site-packages'])"`
-export PYTHON_PATH="$install_dir/$pythonsite_dir:$PYTHON_PATH"
+export PYTHONPATH="$install_dir/$pythonsite_dir:$PYTHON_PATH"
 
 # Checkout roboptim-core
 echo "Installing dependencies..."
@@ -28,6 +28,9 @@ $git_clone "git://github.com/roboptim/roboptim-core.git"
 cd "$core_dir"
 cmake . -DCMAKE_INSTALL_PREFIX:STRING="$install_dir"
 make install
+
+echo $PKG_CONFIG_PATH
+ls "$install_dir/lib/pkgconfig"
 
 # Build package
 echo "Building package..."
