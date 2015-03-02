6ebe3efe128c9dc88adb60f2eefd641c566fe575
Benjamin Chrétien <chretien@lirmm.fr>
2015-03-02 14:45:39 +0100

    tests/finite_differences.py: improve test (check number of callback calls).

--- tests/finite_differences.py
+++ tests/finite_differences.py
@@ -12,9 +12,14 @@ class Square (roboptim.core.PyDifferentiableFunction):
     def __init__ (self):
         roboptim.core.PyDifferentiableFunction.__init__ \
             (self, 1, 1, "square function")
+        self.compute_counter = 0
+
+    def reset (self):
+        self.compute_counter = 0
 
     def impl_compute (self, result, x):
         result[0] = x[0] * x[0]
+        self.compute_counter += 1
 
     def impl_gradient (self, result, x, f_id):
         raise NotImplementedError
@@ -33,16 +38,47 @@ class Problem1_Cost (roboptim.core.PyDifferentiableFunction):
 
 class TestFiniteDifferences(unittest.TestCase):
 
-    def test_jacobian(self):
+    def test_jacobian_simple(self):
 
-        fd_rule = roboptim.core.FiniteDifferenceRule.FIVE_POINTS
-        f = roboptim.core.PyFiniteDifference (Square (), rule = fd_rule)
+        fd_rule = roboptim.core.FiniteDifferenceRule.SIMPLE
+        square = Square()
+        f = roboptim.core.PyFiniteDifference (square, rule = fd_rule)
 
         x = numpy.array ([4.])
         res = f (x)
         numpy.testing.assert_almost_equal (res, [x[0] * x[0]], 5)
+        assert square.compute_counter == 1
+        square.reset ()
+
+        grad = f.gradient (x, 0)
+        numpy.testing.assert_almost_equal (grad, [2. * x[0]], 5)
+        assert square.compute_counter == 2
+        square.reset ()
+
         jac = f.jacobian (x)
         numpy.testing.assert_almost_equal (jac, [[2. * x[0]]], 5)
+        assert square.compute_counter == 2
+
+    def test_jacobian_fivepoints(self):
+
+        fd_rule = roboptim.core.FiniteDifferenceRule.FIVE_POINTS
+        square = Square()
+        f = roboptim.core.PyFiniteDifference (square, rule = fd_rule)
+
+        x = numpy.array ([4.])
+        res = f (x)
+        numpy.testing.assert_almost_equal (res, [x[0] * x[0]], 5)
+        assert square.compute_counter == 1
+        square.reset ()
+
+        grad = f.gradient (x, 0)
+        numpy.testing.assert_almost_equal (grad, [2. * x[0]], 5)
+        assert square.compute_counter == 4
+        square.reset ()
+
+        jac = f.jacobian (x)
+        numpy.testing.assert_almost_equal (jac, [[2. * x[0]]], 5)
+        assert square.compute_counter == 4
 
     def test_problem_1(self):
         """
@@ -58,17 +94,13 @@ class TestFiniteDifferences(unittest.TestCase):
         # Check starting value
         self.assertEqual (cost (problem.startingPoint), 909)
 
-        # Let the test fail if the solver does not exist.
-        try:
-            solver = roboptim.core.PySolver ("ipopt", problem)
-            print (solver)
-            solver.solve ()
-            r = solver.minimum ()
-            print (r)
-            numpy.testing.assert_almost_equal (r.value, [0.], 5)
-            numpy.testing.assert_almost_equal (r.x, [1., 1.], 5)
-        except Exception as e:
-            print ("Error: %s" % e)
+        solver = roboptim.core.PySolver ("ipopt", problem)
+        print (solver)
+        solver.solve ()
+        r = solver.minimum ()
+        print (r)
+        numpy.testing.assert_almost_equal (r.value, [0.], 5)
+        numpy.testing.assert_almost_equal (r.x, [1., 1.], 5)
 
 if __name__ == '__main__':
     unittest.main()
