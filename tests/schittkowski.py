bd7597d5cf45f35cdc12d1035cf45ad6aa9f84c8
Benjamin Chrétien <chretien@lirmm.fr>
2015-04-07 18:04:39 +0900

    Add extra Schittkowski test.

--- tests/schittkowski.py
+++ tests/schittkowski.py
@@ -48,6 +48,45 @@ class Problem6_G1 (roboptim.core.PyDifferentiableFunction):
         result[1] = 10.
 
 
+class Problem8_Cost (roboptim.core.PyDifferentiableFunction):
+    def __init__ (self):
+        roboptim.core.PyDifferentiableFunction.__init__ \
+            (self, 2, 1, "-1")
+
+    def impl_compute (self, result, x):
+        result[0] = -1.
+
+    def impl_gradient (self, result, x, functionId):
+        result[0] = 0.
+        result[1] = 0.
+
+
+class Problem8_G1 (roboptim.core.PyDifferentiableFunction):
+    def __init__ (self):
+        roboptim.core.PyDifferentiableFunction.__init__ \
+            (self, 2, 1, "x₀² + x₁² - 25")
+
+    def impl_compute (self, result, x):
+        result[0] = x[0] * x[0] + x[1] * x[1] - 25.
+
+    def impl_gradient (self, result, x, functionId):
+        result[0] = 2. * x[0]
+        result[1] = 2. * x[1]
+
+
+class Problem8_G2 (roboptim.core.PyDifferentiableFunction):
+    def __init__ (self):
+        roboptim.core.PyDifferentiableFunction.__init__ \
+            (self, 2, 1, "x₀ x₁ - 9")
+
+    def impl_compute (self, result, x):
+        result[0] = x[0] * x[1] - 9.
+
+    def impl_gradient (self, result, x, functionId):
+        result[0] = x[1]
+        result[1] = x[0]
+
+
 class Problem48_Cost (roboptim.core.PyDifferentiableFunction):
     def __init__ (self):
         roboptim.core.PyDifferentiableFunction.__init__ \
@@ -157,6 +196,33 @@ class TestFunctionPy(unittest.TestCase):
         print (r)
         numpy.testing.assert_almost_equal (r.value, [0.])
         numpy.testing.assert_almost_equal (r.x, [1., 1.])
+
+    def test_problem_8(self):
+        """
+        Schittkowski problem #8
+        """
+        cost = Problem8_Cost ()
+        problem = roboptim.core.PyProblem (cost)
+        problem.startingPoint = numpy.array([2., 1., ])
+        problem.argumentBounds = numpy.array([[float("-inf"), float("inf")],
+                                              [float("-inf"), float("inf")], ])
+
+        g1 = Problem8_G1 ()
+        problem.addConstraint (g1, [0., 0.,])
+        g2 = Problem8_G2 ()
+        problem.addConstraint (g2, [0., 0.,])
+
+        # Check starting value
+        numpy.testing.assert_almost_equal (cost (problem.startingPoint)[0], -1.)
+
+        solver = roboptim.core.PySolver ("ipopt", problem)
+        print (solver)
+        solver.solve ()
+        r = solver.minimum ()
+        print (r)
+        a = math.sqrt ((25. + math.sqrt (301.)) / 2.)
+        numpy.testing.assert_almost_equal (r.value, [-1.])
+        numpy.testing.assert_almost_equal (numpy.sort(r.x), numpy.sort([a, 9./a]))
 
     def test_problem_48(self):
         """
